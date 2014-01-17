1c682a76e35f30ad98258975b1e3d0dca2767c01
Sven Pachnit <sven@bmonkeys.net>
2014-01-17 16:50:54 +0100

    chmod screens

--- actions/mcir_default/screen.rb
+++ actions/mcir_default/screen.rb
@@ -4,7 +4,7 @@ class Mcir::Action::Screen < Mcir::Action
   @desc = "capture screenshot of guest OS"
 
   def setup!
-    @config = { capture: 1, interval: 1 }
+    @config = { capture: 1, interval: 1, chmod: true }
     register_options
   end
 
@@ -12,13 +12,15 @@ class Mcir::Action::Screen < Mcir::Action
     @mcir.opt do
       on("-c", "--capture N", Integer, desc_def("number of screens to take", 1)) {|n| act.config[:capture] = n }
       on("-i", "--interval", Float, desc_def("capture interval in seconds", 1)) {|n| act.config[:interval] = n }
+      on("-p", "--preserve-permissions", desc_def("do not chmod screen to 0777")) {act.config[:chmod] = false }
     end
   end
 
   def call instance, args
     @instance = instance
     while true
-      @instance.capture_screenshot!
+      f = @instance.capture_screenshot!
+      FileUtils.chmod(0777, f) if @config[:chmod]
       print "."
       @config[:capture] -= 1
       @config[:capture] > 0 ? sleep(1) : return
--- lib/mcir/instance/io.rb
+++ lib/mcir/instance/io.rb
@@ -3,8 +3,10 @@ class Mcir::Instance
   module IO
     def capture_screenshot!
       dir = "#{home}/screenshots"
+      file = "#{dir}/#{Time.now.strftime("%Y%m%d_%H%M%S")}.png"
       FileUtils.mkdir_p(dir)
-      capture_screen! "#{dir}/#{Time.now.strftime("%Y%m%d_%H%M%S")}.png"
+      capture_screen! file
+      file
     end
   end
 end

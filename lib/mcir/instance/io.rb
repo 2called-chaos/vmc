abe99e2d65d9a898463edf4c995685d2db55eca5
Sven Pachnit <sven@bmonkeys.net>
2014-01-17 16:37:05 +0100

    Fix png filename

--- lib/mcir/instance/io.rb
+++ lib/mcir/instance/io.rb
@@ -4,7 +4,7 @@ class Mcir::Instance
     def capture_screenshot!
       dir = "#{home}/screenshots"
       FileUtils.mkdir_p(dir)
-      capture_screen! "#{dir}/#{Time.now}.png"
+      capture_screen! "#{dir}/#{Time.now.strftime("%Y%m%d_%H%M%S")}.png"
     end
   end
 end
